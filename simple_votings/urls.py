60f4cb7ee3e9608976049d335dcc9ba204b3e5c5
Ivan Yanushkevich <ivanyanush765@gmail.com>
2021-12-21 18:34:52 +0300

    hard code of the vote, but without voting :)

--- simple_votings/urls.py
+++ simple_votings/urls.py
@@ -35,5 +35,6 @@ urlpatterns = [
         ),
         name='login'
     ),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('vote/', views.vote_page, name='vote')
 ]
