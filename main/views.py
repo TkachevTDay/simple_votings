1c338fcfee494c61bf49abc37eaff68084057093
Andrew Smirnov <smirnov@informatics.ru>
2021-12-25 17:26:03 +0300

    Fix vote page

--- main/views.py
+++ main/views.py
@@ -1,6 +1,6 @@
 import datetime
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Voting
 from .models import VoteVariant
 from .models import VoteFact
@@ -24,11 +24,12 @@ def index_page(request):
     }
     return render(request, 'pages/index.html', context)
 
+
 def votings(request):
     votings = []
     users = get_user_model()
     for i in Voting.objects.all():
-        votings.append({'author': users.objects.get(id = i.author), 'voting': i.name, 'id': i.id})
+        votings.append({'author': users.objects.get(id=i.author), 'voting': i.name, 'id': i.id})
     context = {
         'menu': get_menu_context(),
         'votings': votings,
@@ -44,35 +45,21 @@ def time_page(request):
     }
     return render(request, 'pages/time.html', context)
 
-def vote_page(request):
-    vote_id = request.GET.get("vote_id", None)
-    if not vote_id:
-        return render(request, 'pages/404.html')
-    try:
-        vote_content = Voting.objects.get(id = vote_id)
-    except:
-        return render(request, 'pages/404.html')
 
-
-    vote_variants = []
-    for i in VoteVariant.objects.filter(voting=vote_id):
-        vote_variants.append({"id": i.id, "voting": i.voting,"description": i.description})
-
-    users = get_user_model()
+def vote_page(request, vote_id):
+    voting = get_object_or_404(Voting, id=vote_id)
+    vote_variants = VoteVariant.objects.filter(voting=vote_id)
     current_user = request.user
-
-    vote_facts = []
-    for i in VoteFact.objects.filter(user=current_user.id):
-        vote_facts.append(i.variant)
+    vote_facts = VoteFact.objects.filter(user=current_user)
 
     context = {
         'pagename': 'Vote',
         'menu': get_menu_context(),
-        'author': users.objects.get(id=vote_content.author),
-        "vote_name": vote_content.name,
-        "vote_description": vote_content.description,
+        'author': voting.author,
+        "vote": voting,
         "vote_variants": vote_variants,
         "vote_facts": vote_facts,
     }
-    #todo add voting_variants (working)
+
+    # todo: make vote fact
     return render(request, 'pages/vote.html', context)
--- simple_votings/urls.py
+++ simple_votings/urls.py
@@ -35,5 +35,5 @@ urlpatterns = [
         name='login'
     ),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('vote', views.vote_page, name='vote')
+    path('vote/<int:vote_id>/', views.vote_page, name='vote')
 ]
