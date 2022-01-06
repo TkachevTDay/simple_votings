08baad0065feed797ec8244d2feeca4bbbf4d75e
Станислав Делюков <prizrakzamkov@gmail.com>
2022-01-06 13:07:19 +0300

    рабочее голосование (но еще не отредактированное)

--- main/views.py
+++ main/views.py
@@ -27,6 +27,7 @@ def index_page(request):
 
 def votings(request):
     context = {
+        'pagename': 'Голосования',
         'menu': get_menu_context(),
         'votings': Voting.objects.all(),
     }
@@ -42,19 +43,39 @@ def time_page(request):
     return render(request, 'pages/time.html', context)
 
 
+
 def vote_page(request, vote_id):
     voting = get_object_or_404(Voting, id=vote_id)
     vote_variants = VoteVariant.objects.filter(voting=vote_id)
+    vote_facts_variants = []
     current_user = request.user
-    vote_facts = VoteFact.objects.filter(user=current_user, variant__voting=voting)
 
+    if request.method == 'POST':
+        if not current_user.is_anonymous:
+            vote = request.POST.get("VOTE")
+            if (vote):
+                variant = VoteVariant.objects.get(id = vote)
+
+                isexist = VoteFact.objects.filter(user=current_user, variant__voting=voting)# голосовал ли ранее
+                if(not isexist) or voting.type == 2:
+                    if not VoteFact.objects.filter(user=current_user, variant = variant).exists():
+                        VoteFact(user=current_user, variant=variant).save()
+                    else:
+                        print("Голос уже засчитан")
+                else:
+                    print("Голос уже засчитан")
+
+    if not current_user.is_anonymous:
+        vote_facts = VoteFact.objects.filter(user=current_user, variant__voting=voting)
+        for i in vote_facts:
+            vote_facts_variants.append(i.variant)
     context = {
         'pagename': 'Vote page',
         'menu': get_menu_context(),
         'author': voting.author,
         "vote": voting,
         "vote_variants": vote_variants,
-        "vote_fact": vote_facts.first(),
+        "vote_fact": vote_facts_variants,
     }
 
     # todo: make vote fact
